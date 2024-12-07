cc4d9d66f26843a8a3b47b8e1c6753da37ebe586
Searedphantasm <searedphantasm@gmail.com>
2024-12-06 16:54:12 -0800

    first commit -m

---
+++ app/routes/_index.jsx
@@ -0,0 +1,12 @@
+
+
+const _index = () => {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <a href="/demo">Go to demo page.</a>
+        </>
+    );
+};
+
+export default _index;
